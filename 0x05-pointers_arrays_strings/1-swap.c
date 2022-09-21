694b85dd248f6d7a1f6708a1cc61be12d147929f
Ugochinnos <ugochinnos@gmail.com>
2022-09-20 19:17:23 -0700

    my data

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * swap_int - function that swaps the values of two intergers
+ * @a: This is the first entry
+ * @b: This is the second entry
+ *
+ *
+ */
+void swap_int( int *a, int *b)
+{
+	int changed;
+
+     change = *a;
+         *a = *b;
+         *b = changed;
+}
+
